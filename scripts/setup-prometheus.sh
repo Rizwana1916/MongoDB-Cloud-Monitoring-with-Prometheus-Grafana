217689880ac9207a406e551e7ee46e1af303ebb6
Rizwana Sheikh <121599047+Rizwana1916@users.noreply.github.com>
2023-08-13 15:44:20 +0530

    Create setup-prometheus.sh

---
+++ scripts/setup-prometheus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to install and configure Prometheus
+
+# Placeholder installation commands
+echo "Downloading Prometheus..."
+# Example: wget https://github.com/prometheus/prometheus/releases/download/v2.29.1/prometheus-2.29.1.linux-amd64.tar.gz
+# Example: tar xvfz prometheus-2.29.1.linux-amd64.tar.gz
+
+# Placeholder configuration steps
+echo "Configuring Prometheus..."
+# Example: cp prometheus-2.29.1.linux-amd64/prometheus /usr/local/bin/
+# Example: cp prometheus-2.29.1.linux-amd64/promtool /usr/local/bin/
+
+echo "Prometheus installation and configuration completed."
