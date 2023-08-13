6ced25c01c4924ffc3065c302ca1011c0d89456c
Rizwana Sheikh <121599047+Rizwana1916@users.noreply.github.com>
2023-08-13 15:45:04 +0530

    Create setup-grafana.sh

---
+++ scripts/setup-grafana.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to install and configure Grafana
+
+# Placeholder installation commands
+echo "Downloading Grafana..."
+# Example: wget https://dl.grafana.com/oss/release/grafana-8.1.5.linux-amd64.tar.gz
+# Example: tar xvfz grafana-8.1.5.linux-amd64.tar.gz
+
+# Placeholder configuration steps
+echo "Configuring Grafana..."
+# Example: cp -r grafana-8.1.5 /usr/share/grafana
+
+echo "Grafana installation and configuration completed."
